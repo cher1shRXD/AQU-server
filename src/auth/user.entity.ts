99fd2f6c364c7411a8e97819b73ae62d37132512
cher1shRXD <taewoo080401@gmail.com>
2024-08-04 00:52:00 +0900

    feat::board return

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,3 +1,4 @@
+import { Exclude } from "class-transformer";
 import { Board } from "src/boards/board.entity";
 import { BaseEntity, Column, Entity, OneToMany, PrimaryGeneratedColumn, Unique } from "typeorm";
 
@@ -11,6 +12,7 @@ export class User extends BaseEntity {
   username:string;
 
   @Column()
+  @Exclude()
   password:string;
 
   @OneToMany(type=> Board, board => board.author, { eager:false })
