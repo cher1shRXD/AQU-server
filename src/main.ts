69715bab38d93faeea3dbcc443d69b7e31b769c3
cher1shRXD <taewoo080401@gmail.com>
2024-08-04 21:46:11 +0900

    fix::cors

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const serverConfig: { port: number } = config.get('server');
   setupSwagger(app);
   app.enableCors({
-    origin: '*',
+    origin: 'localhost:3000',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
     allowedHeaders: [
