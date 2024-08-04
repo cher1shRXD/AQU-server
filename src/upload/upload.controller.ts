9a8b4fd8142919663f2c8a322da2adb3a1abd474
cher1shRXD <taewoo080401@gmail.com>
2024-08-04 23:32:17 +0900

    feat::download file

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -1,12 +1,17 @@
 import {
   Controller,
+  Get,
+  NotFoundException,
+  Param,
   Post,
+  Res,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
-import { extname } from 'path';
+import { extname, join } from 'path';
+import { Response } from 'express';
 
 @Controller('upload')
 export class UploadController {
@@ -29,4 +34,17 @@ export class UploadController {
       url: `https://file-aqu.o-r.kr/${file.filename}`,
     };
   }
+
+  @Get('download/:filename')
+  async downloadFile(@Param('filename') filename: string, @Res() res: Response) {
+    const filePath = join('/var/www/files', filename);
+
+    try {
+      res.sendFile(filePath);
+    } catch (err) {
+      throw new NotFoundException('File not found');
+    }
+  }
 }
+
+
